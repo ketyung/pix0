3437019fa3199a8954570ac0af7c95829d2d1b43
techchee <ketyung@techchee.com>
2022-11-26 14:23:19 +0800

    update

--- src/views/wallet/WalletView.tsx
+++ src/views/wallet/WalletView.tsx
@@ -26,6 +26,12 @@ export const WalletView : FC = () =>{
     },[SelectedWalletStorage.getSelected()]);
 
     return <div className="m-auto p-10 mt-20 border-2 border-gray-200 rounded-3xl max-w-2xl text-center">
+        
+        <div><button className="bg-gray-200 hover:bg-gray-500 
+        px-4 py-2 float-right mb-8 text-black  
+        font-boiold rounded-full min-w-200" onClick={()=>{
+            setSelectedWallet(undefined);
+        }}>All wallets</button></div>
 
         {selectedWallet ? <WalletIndView pubkey={selectedWallet}/> : <WalletListView/> }
     
@@ -36,7 +42,8 @@ export const WalletView : FC = () =>{
             await genWalletNow();
         }}
         disabled={loading}
-        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full min-w-200">
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 
+        px-4 rounded-full min-w-200">
         {loading ? <Spinner/> : <>Create Wallet</>}
         </button>
         </div>
