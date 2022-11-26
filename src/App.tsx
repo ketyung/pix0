b6a5ebd879cdf695ad3c080ed51698a6f6afdae6
techchee <ketyung@techchee.com>
2022-11-26 17:35:24 +0800

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
 //import { View } from './views/View';
 import { WalletView } from './views/wallet/WalletView';
+import { Provider } from "react-redux";
+import { StateStore } from './utils/sm/StateStore';
 import './App.css';
 
 
+
 function App() {
-  return <WalletView/>;
+  return (
+    <div className="App">
+       <Provider store={StateStore}>
+         <WalletView/>
+       </Provider>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/hooks/useWalletState.ts
@@ -0,0 +1,31 @@
+import { useDispatch, useSelector, shallowEqual } from "react-redux";
+import { Dispatch } from "redux";
+import { useCallback} from "react";
+import { WalletState } from "../utils/sm/WalletStateReducer";
+import { setWalletsCount as setWalletCnt, setSelectedWallet as setSelWallet} from "../utils/sm/WalletActions";
+
+
+export default function useWalletState() {
+
+    const dispatch: Dispatch<any> = useDispatch();
+
+    const setSelectedWallet = useCallback((pubkey : string) => 
+        dispatch(setSelWallet(pubkey)),
+    [dispatch]);
+
+    const setWalletCount = useCallback((count : number) => 
+        dispatch(setWalletCnt(count)),
+    [dispatch]);
+
+    const walletState : WalletState =  useSelector(
+        (_state: any) => {return _state.walletStateReducer;}, shallowEqual
+    );
+
+    const walletsCount : number|undefined = walletState.walletsCount;
+
+    const selectedWalletPubkey : string|undefined = walletState.selectedWalletPubkey;
+
+
+    return {setSelectedWallet, setWalletCount, walletsCount, selectedWalletPubkey} as const;
+
+}
