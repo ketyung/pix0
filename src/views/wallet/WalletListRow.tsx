9572d2124c944589b99f34390bf6149cc6db777f
techchee <ketyung@techchee.com>
2022-11-26 10:41:49 +0800

    update

--- src/views/wallet/WalletListRow.tsx
+++ src/views/wallet/WalletListRow.tsx
@@ -13,7 +13,7 @@ export const WalletListRow : FC <Props> = ({
     wallet, index
 }) =>{
 
-    return <div className="items-left max-w-200 text-ellipsis m-6">
+    return <div className="items-left max-w-200 text-ellipsis m-4 bg-slate-50 hover:bg-slate-200 rounded-3xl p-2">
     <span className="max-w-30 mr-4">{(index ?? 0) + 1}.</span> {shortenStringTo(wallet.pubkey, 20)}
     </div>
 }
