4d94db296ad66163d6a49ad034474e1c3ffc046d
techchee <ketyung@techchee.com>
2022-11-27 09:06:09 +0800

    update

--- src/views/SideBar.tsx
+++ src/views/SideBar.tsx
@@ -1,20 +1,37 @@
 import { FC } from "react";
+import usePage from "../hooks/usePage";
+import { Page } from "../models";
 
 
 export const SideBar : FC = () =>{
+
+    const {setPage, isPage} = usePage();
+
+    const classNamesIfPageIs = ( page : Page) => {
+
+        return isPage(page) ? "text-sky-400" : 
+        "cursor-pointer text-gray-400 hover:text-orange-300"
+
+    }
+
 
     return <div className=" min-h-0 flex-1 flex overflow-hidden">
     <nav aria-label="Sidebar" className="hidden lg:block flex-shrink-0 bg-gray-800 overflow-y-auto">
         <div className="relative w-30 flex space-y-16 flex-col p-3">
 
-            <a href="#" className="text-gray-400 hover:text-orange-300">
+            <a onClick={()=>{
+                setPage(Page.Wallet);
+            }} className={ classNamesIfPageIs(Page.Wallet)}>
                 <div className="flex-shrink-0 inline-flex items-center justify-center w-14">
                     <i className="fa fa-money"></i>
                 </div>
                 <div className="text-center text-xs font-normal ">Wallet</div>
             </a>
 
-            <a href="#" className="text-gray-400 hover:text-orange-300">
+            <a onClick={()=>{
+                setPage(Page.MintNFT);
+            }} className={ 
+                classNamesIfPageIs(Page.MintNFT)}>
                 <div className="flex-shrink-0 inline-flex items-center justify-center w-14">
                     <i className="fa fa-picture-o"></i>
                 </div>
@@ -22,21 +39,27 @@ export const SideBar : FC = () =>{
             </a>
 
 
-            <a href="#" className="text-gray-400 hover:text-orange-300">
+            <a onClick={()=>{
+                setPage(Page.Collectibles);
+            }} className={classNamesIfPageIs(Page.Collectibles)}>
                 <div className="flex-shrink-0 inline-flex items-center justify-center w-14">
                     <i className="fa fa-gift"></i>
                 </div>
                 <div className="text-center text-xs font-normal ">Your Collectibles</div>
             </a>
 
-            <a href="#" className="text-gray-400 hover:text-orange-300">
+            <a onClick={()=>{
+                setPage(Page.CreateCollection);
+            }} className={classNamesIfPageIs(Page.CreateCollection)}>
                 <div className="flex-shrink-0 inline-flex items-center justify-center w-14">
                     <i className="fa fa-file-image-o"></i>
                 </div>
                 <div className="text-center text-xs font-normal ">Create Collection</div>
             </a>
 
-            <a href="#" className="text-gray-400 hover:text-orange-300">
+            <a onClick={()=>{
+                setPage(Page.Market);
+            }} className={classNamesIfPageIs(Page.Market)}>
                 <div className="flex-shrink-0 inline-flex items-center justify-center w-14">
                     <i className="fa fa-exchange"></i>
                 </div>
