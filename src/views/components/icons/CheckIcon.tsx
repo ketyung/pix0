3dbdc7b6183f147875adf1e00f60bf110272fed0
techchee <ketyung@techchee.com>
2022-11-27 11:16:51 +0800

    update

--- src/views/components/icons/CheckIcon.tsx
+++ src/views/components/icons/CheckIcon.tsx
@@ -11,7 +11,7 @@ export const CheckIcon : FC <Props> = ({
     checked, textColorClass
 }) =>{
 
-    return <svg className={`h-6 w-6 inline-block ${(checked ? "text-green-500" : "text-gray-300")} ${textColorClass ?? "text-gray-600"}`}  
+    return <svg className={`h-6 w-6 inline-block ${(checked ? "text-green-500" : textColorClass ?? "text-gray-300")}`}  
     width="24" height="24" viewBox="0 0 24 24" 
     strokeWidth="2" stroke="currentColor" fill="none" 
     strokeLinecap="round" strokeLinejoin="round">
