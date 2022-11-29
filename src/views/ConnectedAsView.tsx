a44bab0e2e89fb6524e71b3e5ebf7ca50ff10ebe
ketyung <ketyung@techchee.com>
2022-11-29 14:08:03 +0800

    update

--- src/views/ConnectedAsView.tsx
+++ src/views/ConnectedAsView.tsx
@@ -2,6 +2,7 @@ import { FC , useState, useCallback, useEffect} from "react";
 import useWalletState from "../hooks/useWalletState";
 import { shortenStringTo, pubkeyOrAddress } from "../utils";
 import { WalletsStorage } from "../utils/local-storage";
+import { DropdownList } from "./components/DropdownList";
 import useXrp from "../hooks/useXrp";
 import { Spinner } from "./components/Spinner";
 
@@ -37,13 +38,17 @@ export const ConnectedAsView : FC = () =>{
         fetchBalance();
     },[selectedWalletPubkey])
 
+    const items = [{title : "Logout"},{title : "Settings"}]
     
   
     return <div className="float-right clear-both bg-gray-900 rounded-3xl text-base 
-    text-sky-200 mr-20 mt-2 p-1 w-1/5 align-text-top cursor-pointer max-w-500">
-    <i className="fa fa-plug" aria-hidden="true"></i> 
+    text-sky-200 mr-20 mt-2 p-1 w-1/5 align-text-top cursor-pointer max-w-500 dropdown relative">
+    <a href="#" id="connectAsButton" className="dropdown-toggle"
+    data-bs-toggle="dropdown" aria-expanded="false" type="button">
+    <><i className="fa fa-plug" aria-hidden="true"></i> 
     <span className="ml-4">{shortenStringTo(
         pubkeyOrAddress(WalletsStorage.get(selectedWalletPubkey ?? "")), 10)}</span> 
-    <span className="ml-4">{loading ? <Spinner/> : <>{balance}</>}</span>
+    <span className="ml-4">{loading ? <Spinner/> : <>{balance}</>}</span></></a>
+    <DropdownList items={items} menuButtonId="connectAsButton"/>
     </div>
 }
--- src/views/components/DropdownList.tsx
+++ src/views/components/DropdownList.tsx
@@ -1,6 +1,39 @@
-import { FC } from "react";
+import { FC, ReactElement} from "react";
 
-export const DropdownList : FC = () =>{
+export interface Item {
 
-    return <></>
+    title? : string,
+
+    icon? : ReactElement,
+
+    action? : () => void, 
+}
+
+
+type Props = {
+
+    menuButtonId? : string, 
+
+    items?: Item[],
+}
+
+
+export const DropdownList : FC <Props> = ({
+    menuButtonId, items 
+}) =>{
+
+    return <ul className="dropdown-menu min-w-max absolute hidden bg-white text-base
+      z-50 float-left py-2 list-none text-left rounded-lg shadow-lg mt-1 hidden m-0
+      bg-clip-padding border-none" aria-labelledby={menuButtonId}>
+        {items?.map((e,i)=>{
+
+              return <li key={`${menuButtonId}_Item${i}`} className="dropdown-item text-sm
+                py-2 px-4 font-normal block w-full whitespace-nowrap
+                bg-transparent text-gray-700 hover:bg-gray-100" onClick={()=>{
+                if (e.action){
+                    e.action();
+                }
+            }}>{e.icon ? <span className="mr-2">{e.icon}</span> :<></>}{e.title}</li>
+        })}
+    </ul>
 }
