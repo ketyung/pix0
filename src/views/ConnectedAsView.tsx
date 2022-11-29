3c69f7a960b034fd00eb8e361fcb2f4ac463ad97
ketyung <ketyung@techchee.com>
2022-11-29 14:14:15 +0800

    update

--- src/views/ConnectedAsView.tsx
+++ src/views/ConnectedAsView.tsx
@@ -1,8 +1,7 @@
 import { FC , useState, useCallback, useEffect} from "react";
 import useWalletState from "../hooks/useWalletState";
 import { shortenStringTo, pubkeyOrAddress } from "../utils";
-import { WalletsStorage } from "../utils/local-storage";
-import { DropdownList } from "./components/DropdownList";
+import { WalletsStorage, WalletPasswordStorage } from "../utils/local-storage";
 import useXrp from "../hooks/useXrp";
 import { Spinner } from "./components/Spinner";
 
@@ -38,17 +37,22 @@ export const ConnectedAsView : FC = () =>{
         fetchBalance();
     },[selectedWalletPubkey])
 
-    const items = [{title : "Logout"},{title : "Settings"}]
+    const signOutNow = () => {
+
+        WalletPasswordStorage.remove();
+        window.location.reload();
+    }
     
   
     return <div className="float-right clear-both bg-gray-900 rounded-3xl text-base 
     text-sky-200 mr-20 mt-2 p-1 w-1/5 align-text-top cursor-pointer max-w-500 dropdown relative">
-    <a href="#" id="connectAsButton" className="dropdown-toggle"
-    data-bs-toggle="dropdown" aria-expanded="false" type="button">
-    <><i className="fa fa-plug" aria-hidden="true"></i> 
+    <i className="fa fa-plug" aria-hidden="true"></i> 
     <span className="ml-4">{shortenStringTo(
         pubkeyOrAddress(WalletsStorage.get(selectedWalletPubkey ?? "")), 10)}</span> 
-    <span className="ml-4">{loading ? <Spinner/> : <>{balance}</>}</span></></a>
-    <DropdownList items={items} menuButtonId="connectAsButton"/>
+    <span className="ml-4">{loading ? <Spinner/> : <>{balance}</>}</span>
+    <i className="ml-2 fa fa-sign-out" aria-hidden="true" onClick={()=>{
+        signOutNow();
+    }}/> 
+    
     </div>
 }
