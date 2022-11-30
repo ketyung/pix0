ede2618f198ee061672c584cbdc195e511492327
techchee <ketyung@techchee.com>
2022-12-01 07:53:08 +0800

    update

--- src/hooks/useService.ts
+++ src/hooks/useService.ts
@@ -1,20 +1,25 @@
 import * as service from "../service";
 import { Collection } from "../models/collection";
 import useWalletState from "./useWalletState";
+import { useState } from "react";
 
 export default function useService()  {
 
 
     const {selectedWalletPubkey} = useWalletState();
 
+    const [loading, setLoading] = useState(false);
+
     const addCollection = async  (collection : Collection,
         completion? : (res : Error|Collection) => void ) => {
         
         if ( selectedWalletPubkey) {
 
+            setLoading(true);
             collection.created_by = selectedWalletPubkey;
 
             await service.addCollection(collection, completion);
+            setLoading(false);
         }
         else {
 
@@ -30,7 +35,10 @@ export default function useService()  {
     : Promise<Collection[]> => {
 
         if ( selectedWalletPubkey) {
+            setLoading(true);
+            
             let c = await service.getCollectionsBy(selectedWalletPubkey , offset, limit);
+            setLoading(false);
             return c; 
         }
 
@@ -38,6 +46,6 @@ export default function useService()  {
     }
 
 
-    return {getCollectionsBy, addCollection} as const ;
+    return {getCollectionsBy, addCollection, loading} as const ;
 
 }
--- src/views/createCollection/PFPCollectionHeaderForm.tsx
+++ src/views/createCollection/PFPCollectionHeaderForm.tsx
@@ -1,13 +1,46 @@
 import { FC, useState } from "react";
 import { TextField } from "../components/TextField";
 import { Select } from "../components/Select";
+import { Spinner } from "../components/Spinner";
 import { Status, Collection } from "../../models/collection";
+import useService from "../../hooks/useService";
 
 
 
 export const PFPCollectionHeaderForm : FC = () =>{
 
     const [collection, setCollection] = useState<Collection>({name : "", created_by: ""});
+
+    const {addCollection, loading} = useService();
+
+    const addCollectionNow = async () => {
+
+        if ( collection.name.trim() === "") {
+
+            window.alert("Collection name is blank!");
+            return; 
+        }
+
+        if ( collection.status?.trim() === "") {
+
+            window.alert("Please choose a status for the collection!");
+            return; 
+        }
+
+        await addCollection(collection, (e)=>{
+
+            if (e instanceof Error) {
+
+                window.alert(`Error: ${e.message}`);
+
+            }
+            else {
+                window.alert('Success!');
+                console.log("res::", e, new Date());
+            }
+        });
+    }
+
 
     return <div className="m-auto p-10 mt-4 border-2 border-gray-200 rounded-3xl w-5/6 text-left">
     <form className="bg-white shadow-md rounded-2xl px-8 pt-6 pb-8 mb-4 mt-4">
@@ -35,7 +68,13 @@ export const PFPCollectionHeaderForm : FC = () =>{
         setCollection({...collection, status : e.target.value});
     }}/>
     </div>
-    
+    <div className="mt-2">
+    <button title="Create Collection" disabled={loading} 
+    className="text-sm min-w-32 font-bold p-2 mb-2 bg-gray-500 rounded text-white" 
+    onClick={async ()=>{
+        await addCollectionNow();
+    }}>{loading ? <Spinner/>  : <>Create Collection</>}</button>
+    </div> 
 
     </form>
 
