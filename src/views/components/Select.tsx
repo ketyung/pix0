cae3e22ff2520ab1f51f1791c90590b0609c1060
techchee <ketyung@techchee.com>
2022-11-30 19:19:19 +0800

    update

--- src/views/components/Select.tsx
+++ src/views/components/Select.tsx
@@ -22,7 +22,7 @@ export const Select : FC <Props> = ({
 }) =>{
 
     return <select className="form-select appearance-none
-    block w-full px-3 py-1.5 text-base
+    block w-72 px-3 py-1.5 text-base
     font-normal text-gray-700 bg-white bg-clip-padding bg-no-repeat
     border border-solid border-gray-300
     rounded transition ease-in-out m-0
