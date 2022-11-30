ba2252f1495f8ae8b4c4f3940fbc3925b9a80b7c
techchee <ketyung@techchee.com>
2022-11-30 16:21:08 +0800

    update

---
+++ src/models/collection.ts
@@ -0,0 +1,67 @@
+export enum Category {
+
+    PFP_COLLECTION,
+
+    NICE_QUOTE_COLLECTION,
+}
+
+
+export interface Collection {
+    
+    id? : string, 
+
+    name : string , 
+
+    description? : string,
+
+    category? : Category,
+    
+    logo? : Buffer, 
+
+    layer_count? : number ,
+
+    media_list : CollectionMedia[],
+    
+    created_by : string, 
+
+    date_created? : Date,
+
+    date_updated? : Date,
+}
+
+
+export interface CollectionMedia {
+
+    name? : string, 
+
+    layer_num : number, 
+
+    max_num_of_media? : number, 
+    
+    medias : Media[],
+
+}
+
+export enum MediaType {
+
+    media_uri = 1 ,
+    
+    text = 2,
+
+    shape = 3, 
+
+    data_uri = 4, 
+
+}
+
+export interface Media {
+
+    type : MediaType,
+
+    value? : string, // value can be uri or another JSON string
+
+    data_url? : Buffer, 
+
+    poc? : number, 
+    
+}
