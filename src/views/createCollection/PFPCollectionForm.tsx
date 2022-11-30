b94c337aa896f5fef770d954ab719ab3b018be44
techchee <ketyung@techchee.com>
2022-11-30 08:13:57 +0800

    update

---
+++ src/views/createCollection/PFPCollectionForm.tsx
@@ -0,0 +1,9 @@
+import { FC } from "react";
+import { UploadField } from "../components/UploadField";
+
+export const PFPCollectionForm : FC = () =>{
+
+    return <div>
+    <UploadField label="Upload Media" id="upload1"/>
+    </div>
+}
--- src/views/createCollection/View.tsx
+++ src/views/createCollection/View.tsx
@@ -1,9 +1,10 @@
 import { FC } from "react";
+import { PFPCollectionForm } from "./PFPCollectionForm";
 
 export const View : FC = () =>{
 
     return <div className="mt-10">
         <h2>Create Your Collection Here</h2>
-        
+        <PFPCollectionForm/>
     </div>
 }
