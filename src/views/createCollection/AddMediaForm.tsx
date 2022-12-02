d260c71fbc33054bdca842f413c28a04f072fb3a
ketyung <ketyung@techchee.com>
2022-12-02 11:08:59 +0800

    update

--- src/views/createCollection/AddMediaForm.tsx
+++ src/views/createCollection/AddMediaForm.tsx
@@ -1,6 +1,7 @@
-import { FC } from "react";
+import { FC, useState } from "react";
 import { UploadField } from "../components/UploadField";
-import { Collection } from "../../models/collection";
+import { TextField, commonTextfieldClassName } from "../components/TextField";
+import { Collection, CollectionMedia, Media, MediaType } from "../../models/collection";
 
 
 type Props = {
@@ -12,13 +13,24 @@ export const AddMediaForm : FC <Props> = ({
     collection
 }) =>{
     
+    const [collectionMedia, setCollectionMedia] = useState<CollectionMedia>({
+        layer_num:0, medias: [], 
+    });
 
-    return <div className="m-auto p-10 mt-4 border-2 border-gray-200 rounded-3xl w-5/6 text-center">
+    return <div className="m-auto p-10 mt-4 border-2 border-gray-300 rounded-3xl w-5/6 text-center">
         <div className="mb-4">Add image to your collection <span className="font-bold">{
             collection?.name 
         }</span></div>
+         <form className="bg-white shadow-md rounded-2xl px-8 pt-6 pb-8 mb-4 mt-4 text-left">
+        <div className="mb-4">
+            <TextField label="Name" labelInline={true}
+            className={commonTextfieldClassName("w-64 inline-block")}
+            defaultValue={`${collection?.item_name_prefix} #001`}/>
+        </div>
         <div className="mb-4">
             <UploadField label="Upload Image/Media" withImagePreview={true}/>
         </div>
+       
+        </form>
     </div>
 }
