05ab7a8ba8ec357f8cf01d307005c76658081db0
techchee <ketyung@techchee.com>
2022-12-03 20:58:11 +0800

    update

--- src/views/createCollection/IndMediaView.tsx
+++ src/views/createCollection/IndMediaView.tsx
@@ -19,7 +19,7 @@ export const IndMediaView : FC <Props> = ({
     return <div className="flex-1 w-64 p-4 inline-block border-2 border-gray-200 m-4 
     rounded-2xl hover:bg-gray-200 hover:cursor-pointer">
        {(index ?? 0) +1}. {media &&
-        <img src={media.medias[0].data_url ? 
+        <img src={media?.medias[0]?.data_url ? 
             Buffer.from(media.medias[0].data_url).toString('base64') :
         placeholder} placeholder={placeholder}
         className="object-scale-down h-64 w-64"/>} 
