3ffc1f2359f304a00306610e5cecf06a2ca84184
techchee <ketyung@techchee.com>
2022-12-04 21:38:28 +0800

    update

--- src/views/createCollection/CollectionMediaView.tsx
+++ src/views/createCollection/CollectionMediaView.tsx
@@ -31,7 +31,7 @@ export const CollectionMediaView : FC <Props> = ({
         fetchCollectionMedia();
     },[]);
 
-    return <div className="m-auto p-2 mt-4 border-2 border-gray-200 rounded-3xl w-5/6 text-center">
+    return <div className="m-auto p-2 mt-4 border-2 border-gray-200 rounded-3xl w-9/12 text-center">
     <div className="mb-2">Media in collection <span className="font-bold">{collection.name}</span></div>
     {
     loading ? <Spinner/> : 
