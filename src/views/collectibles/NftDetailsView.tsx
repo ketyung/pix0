0b25b46fac7a361129d8559990e783188f4dc843
techchee <ketyung@techchee.com>
2022-12-05 18:17:16 +0800

    update

--- src/views/collectibles/NftDetailsView.tsx
+++ src/views/collectibles/NftDetailsView.tsx
@@ -1,9 +1,43 @@
-import { FC } from "react";
+import { fetchAsNFTMedata } from "../../utils";
+import { FC , useState, useCallback, useEffect} from "react";
+import * as xrpl from 'xrpl';
 
 
-export const NftDetailsView : FC = () =>{
+import { AccountNFToken, NFTMetadata } from "../../models";
+
+type Props = {
+
+    nftToken? : AccountNFToken,
+}
+
+
+export const NftDetailsView : FC <Props> = ({
+    nftToken
+}) =>{
+
+    const [mediaURI, setMediaURI] = useState<NFTMetadata>();
+
+    const fetchURI = useCallback( async ()=>{
+
+        let m = await fetchAsNFTMedata( xrpl.convertHexToString(nftToken?.URI ?? ""));
+        setMediaURI(m);
+
+    },[]);
+
+    useEffect(()=>{
+
+        fetchURI();
+    
+    },[nftToken?.URI]);
+
 
     return <div className="mt-2 border-2 border-gray-200 m-4 rounded-2xl">
+     <img src={mediaURI === undefined ? xrpl.convertHexToString(nftToken?.URI ?? "")
+        : mediaURI.image } 
+        className="object-scale-down h-auto w-4/5 mb-2"
+        title={mediaURI?.description ? mediaURI.description : "image..."}/>
+    {mediaURI?.name && <div className="mb-4 font-bold">{mediaURI?.name}</div>}
+    {mediaURI?.description && <div className="mb-4">{mediaURI?.description}</div>}
 
 
     </div>
