ad9864bbad9a24efebec0030486eadd68014bf90
techchee <ketyung@techchee.com>
2022-12-07 07:45:26 +0800

    update

--- src/views/HomeView.tsx
+++ src/views/HomeView.tsx
@@ -29,12 +29,12 @@ export const HomeView : FC = () =>{
 
 
     return  hasPasswd ? <div className="flex flex-row min-h-screen bg-gray-100 text-gray-800">
-        <aside className="sidebar w-30 md:shadow transform -translate-x-full md:translate-x-0 
+        <aside className="sidebar w-36 md:shadow transform -translate-x-full md:translate-x-0 
         transition-transform duration-150 ease-in bg-gray-800">
             <SideBar/>
         </aside>
-        <main className="main flex flex-col flex-grow -ml-64 md:ml-0 
-        transition-all duration-150 ease-in">
+        <main className="main flex flex-col grow -ml-64 md:ml-0 
+        transition-all duration-250 ease-in">
             <MainView/>
         </main>
       </div> : <LoginView setPasswordCreated={setPasswordCreated}/>;
