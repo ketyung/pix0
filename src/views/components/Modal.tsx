f5c8a92e84a90b556a183f96db53b29260347113
techchee <ketyung@techchee.com>
2022-12-08 05:50:02 +0800

    update Modal

--- src/views/components/Modal.tsx
+++ src/views/components/Modal.tsx
@@ -22,12 +22,13 @@ export const Modal : FC <Props> = ({
     const [showModal, setShowModal] = useState(false);
 
     return <>
+    {typeof triggerButton === 'string' ?
     <button className="bg-gray-500 text-white active:bg-gray-600 font-bold 
     text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none 
     focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
       type="button" onClick={() => setShowModal(true)}>
       {triggerButton ?? "Open"}
-    </button>
+    </button> :  triggerButton }
     {showModal ? (
       <>
         <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
