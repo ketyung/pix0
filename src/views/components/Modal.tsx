1068ff76518dcf5a92678b6afd208c57f20a7c67
techchee <ketyung@techchee.com>
2022-12-07 21:47:19 +0800

    update

--- src/views/components/Modal.tsx
+++ src/views/components/Modal.tsx
@@ -36,21 +36,13 @@ export const Modal : FC <Props> = ({
           <div
             className="modal-footer flex flex-shrink-0 flex-wrap items-center justify-end p-4 border-t border-gray-200 rounded-b-md">
             <button type="button" className="px-6
-              py-2.5
-              bg-purple-600
-              text-white
-              font-medium
-              text-xs
-              leading-tight
-              uppercase
-              rounded
-              shadow-md
-              hover:bg-purple-700 hover:shadow-lg
+              py-2.5 bg-purple-600 text-white
+              font-medium text-xs leading-tight
+              uppercase rounded
+              shadow-md hover:bg-purple-700 hover:shadow-lg
               focus:bg-purple-700 focus:shadow-lg focus:outline-none focus:ring-0
               active:bg-purple-800 active:shadow-lg
-              transition
-              duration-150
-              ease-in-out" data-bs-dismiss="modal">Close</button>
+              transition duration-150 ease-in-out" data-bs-dismiss="modal">Close</button>
            { actionButton  && <button type="button" 
            onClick={(e)=>{e.preventDefault();
             
