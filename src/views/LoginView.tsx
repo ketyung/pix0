91b387c0bac52cfa1bdcffc6463b095699d9bc5a
ketyung <ketyung@techchee.com>
2022-12-07 14:50:24 +0800

    update

--- src/views/LoginView.tsx
+++ src/views/LoginView.tsx
@@ -15,7 +15,11 @@ export const LoginView : FC <Props>= ({
 
     const [pass, setPass] = useState<string>();
 
-    const createPassword = () =>{
+    const [rpass, setRpass] = useState<string>();
+
+    const [mode, setMode] = useState(0);
+
+    const signIn = () =>{
 
         if ( pass ) {
 
@@ -25,6 +29,47 @@ export const LoginView : FC <Props>= ({
         }
        
     }
+
+
+    const signInForm = <>
+    <h2 className="text-2xl mb-4 font-bold">Sign In</h2>
+    <div className="mb-4 text-left">
+        <TextField label="Password" type="password" onChange={(e)=>{
+            setPass(e.target.value);
+        }}/>
+    </div>
+    <div className="mb-4">
+        <button title="Login" 
+        className="text-sm w-32 font-bold ml-4 p-2 mb-2 bg-gray-500 rounded text-white" 
+        onClick={(e)=>{
+            e.preventDefault();
+            signIn();
+        }}>Login</button>
+    </div>
+    </>
+
+
+    const createNewForm = <>
+    <h2 className="text-2xl mb-4 font-bold">Create Account</h2>
+    <div className="mb-4 text-left">
+        <TextField label="Password" id="pw" type="password" onChange={(e)=>{
+            setPass(e.target.value);
+        }}/>
+    </div>
+    <div className="mb-4 text-left">
+        <TextField label="Password Again" id="rpw" type="password" onChange={(e)=>{
+            setPass(e.target.value);
+        }}/>
+    </div>
+    <div className="mb-4">
+        <button title="Create Account" 
+        className="text-sm w-32 font-bold ml-4 p-2 mb-2 bg-gray-500 rounded text-white" 
+        onClick={(e)=>{
+            e.preventDefault();
+            
+        }}>Create Account</button>
+    </div>
+    </>
 
     return <div className="m-auto p-10 mt-20 border-1 border-gray-300 rounded-3xl w-3/5 
     shadow-2xl text-center mb-10">
@@ -36,21 +81,12 @@ export const LoginView : FC <Props>= ({
             which you can use to create, mint NFTs and trade them on the marketplace.
           </div>
           <form className="bg-white px-8 pt-6 pb-8 mb-4 mt-4">
-            <h2 className="text-2xl mb-4 font-bold">Sign In</h2>
-            <div className="mb-4 text-left">
-                <TextField label="Password" type="password" onChange={(e)=>{
-                    setPass(e.target.value);
-                }}/>
-            </div>
-            <div className="mb-4">
-                <button title="Login" 
-                className="text-sm w-32 font-bold ml-4 p-2 mb-2 bg-gray-500 rounded text-white" 
-                onClick={(e)=>{
-                    e.preventDefault();
-                    createPassword();
-                }}>Login</button>
-            </div>
-            
+            {mode === 1 ? createNewForm : signInForm}
           </form>
+          <div className="mt-2 text-sm">Haven't create an account yet? 
+          Click <a className="text-blue-500 cursor-pointer" onClick={(e)=>{
+            e.preventDefault();
+            setMode(1);
+          }}>here</a> to create one</div>
     </div>
 }
