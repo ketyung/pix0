0a6a7171d99d0bfa50e4b150d6f2f0431e8e2ec2
techchee <ketyung@techchee.com>
2022-12-08 07:03:23 +0800

    update

--- src/views/collectibles/SaleForm.tsx
+++ src/views/collectibles/SaleForm.tsx
@@ -1,18 +1,21 @@
-import { FC } from "react";
+import { FC, useState } from "react";
 import { shortenStringTo } from "../../utils";
+import { Spinner } from "../components/Spinner";
 import { TextField, commonTextfieldClassName } from "../components/TextField";
 import { AccountNFToken } from "../../models";
 
 type Props = {
     nftToken? : AccountNFToken,
 
-    setPrice? : ( price : number) => void,
-
-    price? : number,
 }
 export const SaleForm : FC <Props> = ({
-    nftToken, setPrice, price
+    nftToken
 }) =>{
+
+    const [ price, setPrice] = useState(1);
+
+    const [processing,setProcessing] = useState(false);
+
 
     return <div className="mt-2 text-left" style={{minWidth:"600px"}}>
         <div className="mb-4">
@@ -23,13 +26,22 @@ export const SaleForm : FC <Props> = ({
         <TextField label="Price: " type="number" labelInline={true} 
         value={`${price}`}
         onChange={(e)=>{
-            if ( setPrice) {
-                let p = parseFloat(e.target.value);
-                if ( !isNaN(p))
-                    setPrice(p);
-            }
+            let p = parseFloat(e.target.value);
+            if ( !isNaN(p))
+                setPrice(p);
+        
         }} className={commonTextfieldClassName('ml-2 w-64')}/><span className="ml-2 font-bold">XRP</span>
         </div>
+        <div className="mb-4">
+        <button title="Burn!!" disabled={processing}
+        className="text-sm w-64 font-bold ml-4 text-2xl p-2 mb-2 bg-gray-800 rounded-3xl text-white" 
+        onClick={async (e)=>{
+            e.preventDefault();
+            
+        }}>{processing ? <Spinner/> : <><i className="fa fa-flag mr-2" aria-hidden="true"/>
+        <span className="mr-6">Create</span></>}</button> 
+        </div>
+    
     </div>
-
+ 
 }
--- src/views/components/Modal.tsx
+++ src/views/components/Modal.tsx
@@ -57,7 +57,7 @@ export const Modal : FC <Props> = ({
                   className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
                   type="button"
                   onClick={() => setShowModal(false)}>
-                  Close
+                  <i className="fa fa-window-close"/>
                 </button>
                 {actionButton &&
                 <button className="bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
