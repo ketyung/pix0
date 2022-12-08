920aeb7f3dc23e06f48ae2b75de89f3d9e98baad
techchee <ketyung@techchee.com>
2022-12-09 06:58:14 +0800

    update

--- src/views/market/OfferListRow.tsx
+++ src/views/market/OfferListRow.tsx
@@ -1,5 +1,6 @@
 import { FC , useState} from "react";
 import { Spinner } from "../components/Spinner";
+import { dateToTimeAgo } from "../../utils";
 import { NFTMetadataImageView } from "../collectibles/NFTMetadataImageView";
 import { NFTMetadata } from "../../models";
 import { Offer } from "../../models/token_offer";
@@ -21,34 +22,45 @@ export const OfferListRow : FC <Props> = ({
 
     const [processing, setProcessing] = useState(false);
 
+    const timeAgo = dateToTimeAgo(offer?.date_created);
+
+
     return <div className="mb-6 p-4 border-b-2 hover:bg-gray-200 rounded-2xl w-1/2 mx-auto shadow-2xl">
       <div className="mb-2 text-left font-bold">
         <span className="mr-2">{((index ?? 0) + 1)}.</span>  
         {metadata?.name && <span className="ml-2">{metadata?.name}</span>}
       </div>
       <div className="mb-2 text-left">
+      <div className="inline-block mt-2">
       <NFTMetadataImageView hexUri={offer?.nft_token?.URI} setMetadataCallback={setMetadata}
-      className="object-scale-down my-2 inline-block" style={{width:"80px",height:"80px"}}/>
+      className="object-scale-down my-2" style={{width:"80px",height:"80px"}}/>
+      </div>
       <div className="inline-block mt-2 ml-2">
-        <div className="mb-2"><span className="mr-2 ml-2">Price:</span><span className="font-bold">{offer?.price ?? 1} XRP</span></div>
-        <button title="Buy..." disabled={processing}
-        className="text-sm w-20 font-bold text-sm p-2 mb-2 bg-gray-800 text-center 
-        rounded-3xl text-white inline-block m-2" 
-        onClick={async (e)=>{
-            e.preventDefault();
-        }}>{processing ? <Spinner/> : <>Buy</>}</button> 
-        <button title="Create Buy Offer..." disabled={processing}
-        className="text-sm w-20 font-bold text-sm p-2 mb-2 bg-purple-600 text-center 
-        rounded-3xl text-white inline-block m-2" 
-        onClick={async (e)=>{
-            e.preventDefault();
-        }}>{processing ? <Spinner/> : <>Offer</>}</button> 
-          <button title="More Details..." disabled={processing}
-        className="text-sm w-20 font-bold text-sm p-2 mb-2 bg-blue-600 text-center 
-        rounded-3xl text-white inline-block m-2" 
-        onClick={async (e)=>{
-            e.preventDefault();
-        }}>{processing ? <Spinner/> : <>Details</>}</button>  
+            <div className="mb-2">
+            <span className="mr-2 ml-2">Price:</span><span className="font-bold">{offer?.price ?? 1} XRP</span>
+            </div>
+            <button title="Buy..." disabled={processing}
+            className="text-sm w-20 font-bold text-sm p-2 mb-2 bg-gray-800 text-center 
+            rounded-3xl text-white inline-block m-2" 
+            onClick={async (e)=>{
+                e.preventDefault();
+            }}>{processing ? <Spinner/> : <>Buy</>}</button> 
+            <button title="Create Buy Offer..." disabled={processing}
+            className="text-sm w-20 font-bold text-sm p-2 mb-2 bg-purple-600 text-center 
+            rounded-3xl text-white inline-block m-2" 
+            onClick={async (e)=>{
+                e.preventDefault();
+            }}>{processing ? <Spinner/> : <>Offer</>}</button> 
+            <button title="More Details..." disabled={processing}
+            className="text-sm w-20 font-bold text-sm p-2 mb-2 bg-blue-600 text-center 
+            rounded-3xl text-white inline-block m-2" 
+            onClick={async (e)=>{
+                e.preventDefault();
+            }}>{processing ? <Spinner/> : <>Details</>}</button>  
+
+            <div className="mb-2 pl-2 text-sm" title={offer?.date_created?.toString()}>
+            <>{timeAgo.short}</>
+            </div>
       </div>
     
       </div>
