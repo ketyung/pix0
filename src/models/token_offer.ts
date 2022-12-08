af7b33012d111db2689be155708b989e6e2ec931
techchee <ketyung@techchee.com>
2022-12-08 11:15:35 +0800

    update

---
+++ src/models/token_offer.ts
@@ -0,0 +1,35 @@
+import { AccountNFToken } from ".";
+
+export enum OfferType {
+
+    Sell,
+
+    Buy,
+}
+
+export interface OfferCreator {
+
+    pubkey? : string,
+
+    classic_address? : string, 
+}
+
+export interface Offer {
+
+    type : OfferType,
+
+    offer_id : string, 
+
+    nft_token : AccountNFToken,
+
+    remark? : string, 
+
+    start_date? : number, // in timestamp,
+
+    end_date? : number, 
+
+    created_by : OfferCreator,
+
+    date_created? : Date, 
+}
+
