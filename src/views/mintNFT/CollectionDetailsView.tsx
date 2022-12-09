ebf942ae12598f7517c5c4742c301a3a6be1e5d9
techchee <ketyung@techchee.com>
2022-12-09 16:28:15 +0800

    update

--- src/views/mintNFT/CollectionDetailsView.tsx
+++ src/views/mintNFT/CollectionDetailsView.tsx
@@ -1,6 +1,9 @@
 import { FC, useEffect, useState, useCallback } from "react";
 import { Props } from "./CollectionView";
 import { Spinner } from "../components/Spinner";
+import { MessageView } from "../components/MessageView";
+import { Message, MessageType } from "../../models";
+import useXrp from "../../hooks/useXrp";
 import useService from "../../hooks/useService";
 
 
@@ -14,6 +17,8 @@ export const CollectionDetailsView : FC <Props> = ({
 
     const [processing, setProcessing] = useState(false);
 
+    const [message, setMessage] = useState<Message>();
+
     const fetchMedia = useCallback(async ()=>{
 
         let m = await getOneCollectionMedia(collection?.id ?? "");
@@ -23,17 +28,43 @@ export const CollectionDetailsView : FC <Props> = ({
 
     },[getOneCollectionMedia]);
 
+    const {randomMint} = useXrp();
+
+    const setMessageNow = (message : Message) => {
+
+        setMessage(message);
+        setTimeout(()=>{
+            setMessage(undefined);
+        }, 5000);
+    }
+
+
     useEffect(()=>{
         fetchMedia();
     },[]);
 
     const mintNow = async () => {
 
+        if ( collection) {
+
+            await randomMint(collection, (e)=>{
+
+                if ( e instanceof Error){
+
+                    setMessageNow({text : e.message, type : MessageType.Error});
+                }
+                else {
+
+                    setMessageNow({text: "Success!", type: MessageType.Info, hash : e});
+                }
+            });
+        }
+       
     }
 
     return <div className="mt-10 text-center w-3/5 p-10 rounded-3xl 
     mx-auto bg-gray-100 hover:bg-gray-300 shadow-2xl">
-
+        {message && <MessageView message={message}/>}
         <div title={collection?.name} 
         className="mb-2 text-left pl-1 font-bold text-lg w-100 line-clamp-1">{collection?.name}</div>
         <div className="mb-2 text-left pl-1 line-clamp-2 w-100 text-sm"
