088a40f48c68e6c6b11858dbd2e565357ec808f2
techchee <ketyung@techchee.com>
2022-12-10 15:55:51 +0800

    update

--- src/views/HomeView.tsx
+++ src/views/HomeView.tsx
@@ -29,7 +29,7 @@ export const HomeView : FC = () =>{
 
 
     return  hasPasswd ? <div className="flex flex-row min-h-screen bg-gray-100 text-gray-800">
-        <aside className="sidebar w-36 md:shadow transform -translate-x-full md:translate-x-0 
+        <aside style={{minWidth:"130px"}} className="sidebar w-32 md:shadow transform -translate-x-full md:translate-x-0 
         transition-transform duration-150 ease-in bg-gray-800">
             <SideBar/>
         </aside>
