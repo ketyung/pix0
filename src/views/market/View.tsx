8ed933d4ee1feee4da8d4bb7d782780422c27d26
techchee <ketyung@techchee.com>
2022-12-10 14:54:31 +0800

    update

--- src/views/market/View.tsx
+++ src/views/market/View.tsx
@@ -4,7 +4,7 @@ import { SellOffersView } from "./SellOffersView";
 export const View : FC = () =>{
 
     return <div className="mt-20">
-        <h1 className="font-bold">Buy/Sell your NFT</h1>
+        <h1 className="font-bold">Buy NFT</h1>
         <SellOffersView/>        
     </div>
 }
