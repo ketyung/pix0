3120db03aab8d86a51dd9ce2e4ca70bccf1c952c
techchee <ketyung@techchee.com>
2022-12-10 15:42:43 +0800

    update

--- src/views/mintNFT/PubCollectionsView.tsx
+++ src/views/mintNFT/PubCollectionsView.tsx
@@ -1,6 +1,7 @@
 import { FC, useCallback, useEffect, useState } from "react";
 import useService from "../../hooks/useService";
 import { ViewType } from "./View";
+import { Spinner } from "../components/Spinner";
 import { Collection } from "../../models/collection";
 import { CollectionView } from "./CollectionView";
 
@@ -17,12 +18,18 @@ export const PubCollectionsView : FC <Props> = ({
 
     const [collections, setCollections] = useState<Collection[]>();
 
+    const [loading, setLoading] = useState(false);
+
     const {getPublishedCollections} = useService();
 
     const fetchPublishedCollections = useCallback(async ()=>{
+
+        setLoading(true);
         let c = await getPublishedCollections();
         if ( c )
             setCollections(c.res);
+
+        setLoading(false);
 
     },[getPublishedCollections]);
 
@@ -30,8 +37,8 @@ export const PubCollectionsView : FC <Props> = ({
         fetchPublishedCollections();
     },[]);
 
-    return <div className="mx-auto p-10 mt-4 border-2 border-gray-200 rounded-3xl w-5/6 text-left">
-    {
+    return <div className="mx-auto p-10 mt-4 border-2 border-gray-200 rounded-3xl w-5/6 text-center">
+    { loading ? <div className="mt-10 w-32 mx-auto p-10 text-center mb-10"><Spinner/></div> :
         collections?.map((c,i)=>{
             return <CollectionView collection={c} key={`coll_${i}`} setViewType={setViewType}/>
         })
